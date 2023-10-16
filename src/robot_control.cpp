9ca660d40a96af42d3640398e4c27daa301c64e8
AnbangChen <axc1293@case.edu>
2023-10-16 00:30:41 -0700

    v1.3

--- src/robot_control.cpp
+++ src/robot_control.cpp
@@ -7,10 +7,10 @@ void laserScan_callback(const sensor_msgs::LaserScan::ConstPtr &msgs);
 void desvel_callback(const geometry_msgs::Twist::ConstPtr &msgs);
 
 // Globals
-bool manual_control_enable = false;
 bool hit = false;
 sensor_msgs::LaserScan laser_data;
 geometry_msgs::Twist cmd_vel;
+
 
 // Callback
 void laserScan_callback(const sensor_msgs::LaserScan::ConstPtr &msgs) {
@@ -18,7 +18,6 @@ void laserScan_callback(const sensor_msgs::LaserScan::ConstPtr &msgs) {
 }
 
 void desvel_callback(const geometry_msgs::Twist::ConstPtr &msgs) {
-    manual_control_enable = true;
     cmd_vel = *msgs;
     ROS_INFO("Manual Command: Forward velocity %2.2f; Angular velocity %2.2f", msgs->linear.x, msgs->angular.z);
 }
@@ -33,7 +32,7 @@ int main(int argc, char **argv) {
     ros::Rate loop_rate(10);
     
     // Initial speed
-    cmd_vel.linear.x = 0.1;
+    cmd_vel.linear.x = 0.2;
     cmd_vel.linear.y = 0;
     cmd_vel.linear.z = 0;
     cmd_vel.angular.x = 0;
@@ -42,6 +41,8 @@ int main(int argc, char **argv) {
     
     // Init wall_dist with a default value
     double wall_dist = 1.0;
+    // control the sensitivity of sensors when detecting hit
+    double detect_sensitivity  = 45.0;
     ROS_INFO_ONCE("wall_dist began with: [%2.2f]", wall_dist);
     
     if (publisher_handle.getParamCached("/wall_dist", wall_dist)) {
@@ -51,7 +52,7 @@ int main(int argc, char **argv) {
     ROS_INFO_ONCE("wall_dist is now: [%2.2f]", wall_dist);
 
     // Detect angle of the laser, consider data within [-1/2*PI, 1/2*PI] to avoid collision
-    const float detect_angle = 45 * 3.14 / 180;
+    const double detect_angle = detect_sensitivity * 3.14 / 180;
     
     while (ros::ok()) {
         hit = false;
@@ -74,14 +75,11 @@ int main(int argc, char **argv) {
                 cmd_vel.linear.x = 0;
                 cmd_vel.angular.z = 0.2;            
                 ROS_WARN("Turning");
-                manual_control_enable = false;
             } 
             else {
-                if (!manual_control_enable) {
-                    cmd_vel.linear.x = 0.2;
-                    cmd_vel.angular.z = 0;
-                    ROS_INFO("Moving Forward");
-                } 
+            	cmd_vel.linear.x = 0.2;
+            	cmd_vel.angular.z = 0;
+           	ROS_INFO("Moving Forward");
             }
             p_pub.publish(cmd_vel);
         } 
